86873a77d868bdd6e06c268c313e793fd1b0840a
Sehyeon Kim <sshyeoniz@naver.com>
2026-07-15 11:04:34 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- Python3/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
+++ Python3/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -1,22 +1,10 @@
-# 지폐를 접을 때는 항상 길이가 긴 쪽을 반으로 접습니다. 
-# 접기 전 길이가 홀수였다면 접은 후 소수점 이하는 버립니다. => round()
-# 접힌 지폐를 그대로 또는 90도 돌려서 지갑에 넣을 수 있다면 그만 접습니다. => 길이가 둘 다 작으면 그만!
-
 def solution(wallet, bill):
-    answer = 0
-    
-    wallet.sort()
-    bill.sort()
-    
-    while (bill[0] > wallet[0] or bill[1] > wallet[1]):
-        if bill[0] > bill[1]:
-            bill[0] //= 2
-            answer += 1
-        else:
-            bill[1] //= 2
-            answer += 1
-        wallet.sort()
-        bill.sort()
-        
-    
-    return answer
+
+    wallet, bill = sorted(wallet), sorted(bill)
+    cnt = 0
+    while wallet[0] < bill[0] or wallet[1] < bill[1]:
+        bill[-1] //= 2
+        bill = sorted(bill)
+        cnt += 1
+
+    return cnt
