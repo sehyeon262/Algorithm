1f691a2967ee574c251ee79b38533096b3749b1b
Sehyeon Kim <sshyeoniz@naver.com>
2026-08-19 13:20:24 +0900

    [level 2] Title: 더 맵게, Time: 1627.79 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,10 +1,11 @@
 from heapq import heapify, heappop, heappush
 
 def solution(scoville, K):
-    # 기존 리스트를 힙으로 변환
+    # 기존 리스트 자체를 힙으로 변환
     heapify(scoville)
     
     cnt = 0
+    # heapq는 최솟값만 항상 맨 앞에 오도록 유지! 완전히 정렬되는 건 아님 XXXXX
     while scoville[0] < K:
         if len(scoville) < 2:
             return -1
